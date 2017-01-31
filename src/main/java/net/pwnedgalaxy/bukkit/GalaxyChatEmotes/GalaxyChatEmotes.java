0378c0f2b88f378cea3360501c476b935c86d542
Austin Peterson <austin@akpwebdesign.com>
2017-01-31 00:43:18 -0500

    jimmy, fiteme, and goodshit commands.

--- src/main/java/net/pwnedgalaxy/bukkit/GalaxyChatEmotes/GalaxyChatEmotes.java
+++ src/main/java/net/pwnedgalaxy/bukkit/GalaxyChatEmotes/GalaxyChatEmotes.java
@@ -15,6 +15,9 @@ public class GalaxyChatEmotes extends JavaPlugin
         getCommand("tablethrow").setExecutor(new GalaxyEmoteCommand("(ノಠ益ಠ)ノ彡┻━┻"));
         getCommand("ohwell").setExecutor(new GalaxyEmoteCommand("ᕕ( ᐛ )ᕗ"));
         getCommand("kawaii").setExecutor(new GalaxyEmoteCommand("ｶﾜ(・∀・)ｲｲ!!"));
+        getCommand("fiteme").setExecutor(new GalaxyEmoteCommand("(ง ͠° ͟ل͜ ͡°)ง"));
+        getCommand("jimmy").setExecutor(new GalaxyEmoteCommand("[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]"));
+        getCommand("goodshit").setExecutor(new GalaxyEmoteCommand("👌👀👌👀👌👀👌👀👌👀 good shit go౦ԁ sHit👌 thats ✔ some good👌👌shit right👌👌there👌👌👌 right✔there ✔✔if i do ƽaү so my self 💯 i say so 💯 thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ💯 👌👌 👌НO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ👌 👌👌 👌 💯 👌 👀 👀 👀 👌👌Good shit"));
         
         PluginDescriptionFile pdfFile = this.getDescription();
         getLogger().info(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
